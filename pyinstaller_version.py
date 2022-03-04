4fdcd45bc903ff4c835c5506e799f4d5a2b8cbc1
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-04 08:28:55 +0100

    Add a script to create a version file
    Added the script to create the version file for pyinstaller.
    The version file is used to save the version to the newly created exe
    file.

---
+++ pyinstaller_version.py
@@ -0,0 +1,12 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="versionfile.txt",
+    version="1.0.0.0",
+    company_name="OlkWis",
+    file_description="Alien Invasion game",
+    internal_name="Alien Invasion",
+    legal_copyright="OlkWis. All rights reserved.",
+    original_filename="alien_invasion.exe",
+    product_name="Simple Game"
+)
