f6a1c1c3a4bab687b314d697b75a0309915ea16a
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-21 09:38:49 +0100

    Add a script to list typed passwords to the network
    The script for the listing of the entered passwords to the network has
    been added.

---
+++ wifi_password_scraper_.py
@@ -0,0 +1,21 @@
+#      ******   Wifi Password Scrapper   ******
+
+import subprocess
+
+data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
+
+# Let's store the profiles by converting them to a list
+profiles = [value.split(':')[1][1:-1] for value in data if "All User Profile" in value]
+
+for profile in profiles:
+  password = subprocess.check_output(['netsh','wlan','show','profile',profile,'key=clear']).decode('utf-8').split('\n')
+
+# Now sort passwords
+
+  password = [passwd.split(':')[1][1:-1] for passwd in password if "Key Content" in passwd]
+
+# We are all set, now lets print out passwords
+  try:
+    print("{:<30}|  {:<}".format(profile,password[0]))
+  except IndexError:
+    print("{:<30}|  {:<}".format(profile, ""))
