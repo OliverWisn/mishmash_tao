87d8059253ef55da683534b876d05db901d2de7c
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-07-22 07:41:47 +0200

    Add learning quality measures
    The various plots and the printed data allow to evaluate the quality
    of the artificial intelligence learning.

--- big_data_and_ai/k_n_neigh_dig_dset.py
+++ big_data_and_ai/k_n_neigh_dig_dset.py
@@ -3,8 +3,14 @@
 
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 # Loading the Dataset.
 digits = load_digits()
@@ -22,6 +28,9 @@ for item in zip(axes.ravel(), digits.images, digits.target):
     axes.set_yticks([])
     axes.set_title(target)
 plt.tight_layout()
+
+# Diagram display.
+plt.show()
 
 # Splitting the Data for Training and Testing.
 X_train, X_test, y_train, y_test = train_test_split(
@@ -41,5 +50,34 @@ expected = y_test
 wrong = [(p, e) for (p, e) in zip(predicted, expected) if p != e]
 print('Wrong predictions in tuples:', wrong)
 
-# Diagram display.
+# Calculating the accuracy of the prediction.
+print('Accuracy of the prediction:', f'{knn.score(X_test, y_test):.2%}')
+
+# Confusion matrix.
+misses = confusion_matrix(y_true=expected, y_pred=predicted)
+print('\nConfusion matrix:')
+print(misses)
+
+# Classification report.
+names = [str(digit) for digit in digits.target_names]
+print('\nClassification report:')
+print(classification_report(expected, predicted, target_names=names))
+
+# Visualization of the confusion matrix.
+misses_df = pd.DataFrame(misses, index=range(10), columns=range(10))
+axes = sns.heatmap(misses_df, annot=True, cmap='nipy_spectral_r')
+
+# Display of the heatmap of the confusion matrix.
 plt.show()
+
+# KFold Class.
+kfold = KFold(n_splits=10, random_state=11, shuffle=True)
+
+# Using the KFold Object with Function cross_val_score
+scores = cross_val_score(estimator=knn, X=digits.data, y=digits.target, 
+    cv=kfold)
+print("""\nThe accuracy of the model's predictions for particular folds for 
+    KFold object:""")
+print(scores)
+print(f'Mean accuracy: {scores.mean():.2%}')
+print(f'Accuracy standard deviation: {scores.std():.2%}')
