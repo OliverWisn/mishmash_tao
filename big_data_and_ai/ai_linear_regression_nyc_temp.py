0e15fbf01d20de1ec9b4f111220bac87ba98faed
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2023-05-29 14:46:08 +0200

    Train the Model
    Trained the Model by the LinearRegression estimator.

--- big_data_and_ai/ai_linear_regression_nyc_temp.py
+++ big_data_and_ai/ai_linear_regression_nyc_temp.py
@@ -1,6 +1,7 @@
 # ai_linear_regression_nyc_temp.py
 # Time Series and Simple Linear Regression
 
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
@@ -9,6 +10,7 @@ nyc = pd.read_csv('ave_hi_nyc_jan_1895-2018.csv')
 nyc.columns = ['Date', 'Temperature', 'Anomaly']
 nyc.Date = nyc.Date.floordiv(100)
 print(nyc.head(3))
+print('------------')
 
 # Splitting the Data for Training and Testing
 X_train, X_test, y_train, y_test = train_test_split(
@@ -18,3 +20,14 @@ print(f'Shape of X data to train: {X_train.shape}')
 print(f'Shape of X data to test: {X_test.shape}')
 print(f'Shape of y data to train: {y_train.shape}')
 print(f'Shape of y data to test: {y_test.shape}')
+print('------------')
+
+# Training the Model
+linear_regression = LinearRegression()
+linear_regression.fit(X=X_train, y=y_train)
+print('The value of the parameter m calculated by the estimator for' +
+		' the equation y=mx+b:')
+print(linear_regression.coef_)
+print('The value of the parameter b calculated by the estimator for' +
+		' the equation y=mx+b:')
+print(linear_regression.intercept_)
